fab206c22833e57488c588e16c97991a5bf52e9f
kamikite <quang.d.chau@gmail.com>
2015-08-18 14:42:13 +0200

    BattleActivity
    XML Layout hinzugefügt

--- LocationBasedAdventure/app/src/main/java/com/example/markus/locationbasedadventure/BattleActivity.java
+++ LocationBasedAdventure/app/src/main/java/com/example/markus/locationbasedadventure/BattleActivity.java
@@ -10,22 +10,42 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+
+import com.example.markus.locationbasedadventure.AsynchronTasks.UpdateHitpointsbarTask;
 
 /**
  * Created by qcd on 14.08.2015.
  */
 public class BattleActivity extends Activity{
 
+    ProgressBar playerHitpoints;
+    final int payers_turn = 0;
+    final int nonplayers_turn = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_battle);
+        initProgressbars();
 
 
 
     }
+
+    private void initProgressbars() {
+        playerHitpoints = (ProgressBar) findViewById(R.id.progressBar);
+        playerHitpoints.setMax(100);
+        playerHitpoints.setProgress(80);
+
+    }
+
+    private void updateProgressbar() {
+
+       // new UpdateHitpointsbarTask(this).execute(0);
+    }
+
 
     public void loadBattleData(){
         //Laden aller relevanten Kampfdaten
