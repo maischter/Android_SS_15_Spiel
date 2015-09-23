e4143dd53d6361ca99b716124925ea7ff173cbdf
maischter <philipp.stefan.mai@googlemail.com>
2015-09-23 15:43:32 +0200

    Current location shown on map
    next will be the location update and camer following

--- LocationBasedAdventure/app/src/main/java/com/example/markus/locationbasedadventure/MapsActivity.java
+++ LocationBasedAdventure/app/src/main/java/com/example/markus/locationbasedadventure/MapsActivity.java
@@ -1,8 +1,14 @@
 package com.example.markus.locationbasedadventure;
 
-import android.support.v4.app.FragmentActivity;
+import android.content.Context;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.util.Log;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
@@ -17,7 +23,9 @@ public class MapsActivity extends FragmentActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         setUpMapIfNeeded();
+        goWithTheFlow();
     }
+
 
     @Override
     protected void onResume() {
@@ -60,6 +68,37 @@ public class MapsActivity extends FragmentActivity {
      * This should only be called once and when we are sure that {@link #mMap} is not null.
      */
     private void setUpMap() {
-        mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+        mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker").snippet("Snippet"));
+
+        mMap.setMyLocationEnabled(true);
+
+        LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        Criteria cr = new Criteria();
+
+        String provider = lm.getBestProvider(cr, true);
+
+        Location myLocation = lm.getLastKnownLocation(provider);
+
+        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+
+        double lat = myLocation.getLatitude();
+        double lng = myLocation.getLongitude();
+
+        LatLng latLng = new LatLng(lat, lng);
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(18));
+
+        mMap.getUiSettings().setScrollGesturesEnabled(false);
+
+        Log.d("latlng", "lat: " + lat + ", lng: " + lng);
+
+        //mMap.addMarker(new MarkerOptions().position(new LatLng(lat, lng)).title("Momentaner Standort").snippet("Hat wohl geklappt!"));
+
+    }
+
+    private void goWithTheFlow() {
     }
 }
