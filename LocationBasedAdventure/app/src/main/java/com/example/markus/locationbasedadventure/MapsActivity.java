0ffbd8860e2f69190c028a6eb9ac82a09e5a1c12
maischter <philipp.stefan.mai@googlemail.com>
2015-09-24 17:40:38 +0200

    Reworked nearly everything.
    Sollte jetzt alles so klappen wie gewünscht. Jetzt kommt dann die
    Gegnererkennung dran...

--- LocationBasedAdventure/app/src/main/java/com/example/markus/locationbasedadventure/MapsActivity.java
+++ LocationBasedAdventure/app/src/main/java/com/example/markus/locationbasedadventure/MapsActivity.java
@@ -1,38 +1,28 @@
 package com.example.markus.locationbasedadventure;
 
-import android.content.Context;
 import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
-import android.util.Log;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.util.Random;
 
-public class MapsActivity extends FragmentActivity {
+public class MapsActivity extends FragmentActivity implements LocationListener{
 
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
-
-    protected Location mLocation;
 
     private double lat, lng;
     private LatLng latLng;
 
-    private LocationListener mLocationListener;
-
-    private boolean followPlayer;
-
     private LatLng[] enemies = new LatLng[5];
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -72,9 +62,36 @@ public class MapsActivity extends FragmentActivity {
             // Check if we were successful in obtaining the map.
             if (mMap != null) {
                 setUpMap();
-
             }
         }
+    }
+
+
+    /**
+     * This is where we can add markers or lines, add listeners or move the camera. In this case, we
+     * just add a marker near Africa.
+     * <p/>
+     * This should only be called once and when we are sure that {@link #mMap} is not null.
+     */
+    private void setUpMap() {
+        mMap.setMyLocationEnabled(true);
+        mMap.getUiSettings().setAllGesturesEnabled(false);
+        mMap.getUiSettings().setMyLocationButtonEnabled(false);
+        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+        Criteria cr = new Criteria();
+
+        String provider = locationManager.getBestProvider(cr, true);
+
+        Location location = locationManager.getLastKnownLocation(provider);
+
+        if(location!=null){
+            onLocationChanged(location);
+        }
+        locationManager.requestLocationUpdates(provider, 1000, 0, this);
+
+        setupEnemies();
+
     }
 
     private void setupEnemies() {
@@ -86,10 +103,10 @@ public class MapsActivity extends FragmentActivity {
 
     private LatLng getRandomLat() {
         LatLng result;
-        double rangeMin = 0.001;
-        double rangeMax = 0.01;
-        double elat = generateRandomDouble(rangeMin, rangeMax);
-        double elng = generateRandomDouble(rangeMin, rangeMax);
+        double rangeMin = -0.001;
+        double rangeMax = 0.001;
+        double elat = generateRandomDouble(rangeMin, rangeMax) + lat;
+        double elng = generateRandomDouble(rangeMin, rangeMax) + lng;
         result = new LatLng(elat, elng);
         return result;
     }
@@ -100,79 +117,29 @@ public class MapsActivity extends FragmentActivity {
         return randomValue;
     }
 
-    /**
-     * This is where we can add markers or lines, add listeners or move the camera. In this case, we
-     * just add a marker near Africa.
-     * <p/>
-     * This should only be called once and when we are sure that {@link #mMap} is not null.
-     */
-
-
-    // Menu in die Ecke
-    private void setUpMap() {
-        //mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker").snippet("Snippet"));
-
-        mMap.setMyLocationEnabled(true);
-
-        LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-
-        Criteria cr = new Criteria();
-
-        mLocationListener = new MLocationListener();
-
-        lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 500, 0, mLocationListener);
-
-        String provider = lm.getBestProvider(cr, true);
-
-        mLocation = lm.getLastKnownLocation(provider);
-
-        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
-
-        updateLoc();
-
-        mMap.moveCamera(CameraUpdateFactory.newCameraPosition(CameraPosition.fromLatLngZoom(latLng, 18)));
-
-        //mMap.animateCamera(CameraUpdateFactory.zoomTo(18));
-
-        //mMap.getUiSettings().setScrollGesturesEnabled(false);
-        //mMap.getUiSettings().setAllGesturesEnabled(false);
-
-        Log.d("latlng", "lat: " + lat + ", lng: " + lng);
-        followPlayer = true;
-
-        //mMap.addMarker(new MarkerOptions().position(new LatLng(lat, lng)).title("Momentaner Standort").snippet("Hat wohl geklappt!"));
-        setupEnemies();
-    }
-
-    private void updateLoc() {
-        lat = mLocation.getLatitude();
-        lng = mLocation.getLongitude();
+    @Override
+    public void onLocationChanged(Location location) {
+        lat = location.getLatitude();
+        lng = location.getLongitude();
         latLng = new LatLng(lat, lng);
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(18));
     }
 
-    private class MLocationListener implements LocationListener {
-        @Override
-        public void onLocationChanged(Location location) {
-            if(followPlayer) {
-                updateLoc();
-                mMap.moveCamera(CameraUpdateFactory.newCameraPosition(CameraPosition.fromLatLngZoom(latLng, 18)));
-                //mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
-            }
-        }
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
 
-        @Override
-        public void onStatusChanged(String provider, int status, Bundle extras) {
+    }
 
-        }
+    @Override
+    public void onProviderEnabled(String provider) {
 
-        @Override
-        public void onProviderEnabled(String provider) {
+    }
 
-        }
+    @Override
+    public void onProviderDisabled(String provider) {
 
-        @Override
-        public void onProviderDisabled(String provider) {
-
-        }
     }
 }
