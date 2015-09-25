a8a0cccab9b6eaf6db56ab42d165a96769f3feb8
maischter <philipp.stefan.mai@googlemail.com>
2015-09-25 22:48:30 +0200

    Kleinere Anpassungen, Verbinungsprobleme abfangen

--- LocationBasedAdventure/app/src/main/java/com/example/markus/locationbasedadventure/MapsActivity.java
+++ LocationBasedAdventure/app/src/main/java/com/example/markus/locationbasedadventure/MapsActivity.java
@@ -1,18 +1,24 @@
 package com.example.markus.locationbasedadventure;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
 import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.view.View;
+import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.Circle;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.util.Random;
 
@@ -25,26 +31,22 @@ public class MapsActivity extends FragmentActivity implements LocationListener{
 
     private String provider;
     private LocationManager locationManager;
-    private Location location;
-    private Criteria cr;
 
     private double lat, lng;
-    private LatLng latLng;
 
     private LatLng[] enemies = new LatLng[NUM_ENEMIES];
     private boolean enemiesSet = false;
+
+    private TextView gpsText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+        setUpMapIfNeeded();
+        setupUI();
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        setUpMapIfNeeded();
-    }
 
     @Override
     protected void onPause() {
@@ -57,6 +59,14 @@ public class MapsActivity extends FragmentActivity implements LocationListener{
     protected void onResume() {
         super.onResume();
         locationManager.requestLocationUpdates(provider, LOC_UPDATE_TIME, 0, this);
+        gpsText.setVisibility(View.VISIBLE);
+        if ( !locationManager.isProviderEnabled( LocationManager.GPS_PROVIDER ) ) {
+            buildAlertMessageNoGps();
+        }
+    }
+
+    private void setupUI() {
+        gpsText = (TextView) findViewById(R.id.gpsText);
     }
 
 
@@ -76,29 +86,68 @@ public class MapsActivity extends FragmentActivity implements LocationListener{
 
     private void setUpMap() {
         mMap.setMyLocationEnabled(true);
-        //mMap.getUiSettings().setAllGesturesEnabled(false);
+        mMap.getUiSettings().setAllGesturesEnabled(false);
         mMap.getUiSettings().setMyLocationButtonEnabled(false);
         locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
 
-        cr = new Criteria();
+        Criteria cr = new Criteria();
 
         provider = locationManager.getBestProvider(cr, true);
 
-        location = locationManager.getLastKnownLocation(provider);
+        Location location = locationManager.getLastKnownLocation(provider);
 
-        if(location!=null){
+        if(location !=null){
             onLocationChanged(location);
         }
         locationManager.requestLocationUpdates(provider, 1000, 0, this);
 
     }
 
+    private void buildAlertMessageNoGps() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Die Standortbestimmung ist momentan deaktiviert. Um spielen zu können müssen Sie diese aktivieren. Wollen Sie das nun tun?")
+                .setCancelable(false)
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    public void onClick(@SuppressWarnings("unused") final DialogInterface dialog, @SuppressWarnings("unused") final int id) {
+                        startActivity(new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    public void onClick(final DialogInterface dialog, @SuppressWarnings("unused") final int id) {
+                        dialog.cancel();
+                        startActivity(new Intent(MapsActivity.this, MenueActivity.class));
+                    }
+                });
+        final AlertDialog alert = builder.create();
+        alert.show();
+    }
+
     private void setupEnemies() {
         for(int i = 0; i < NUM_ENEMIES; i++) {
-            enemies[i] = getRandomLoc();
-            mMap.addMarker(new MarkerOptions().position(enemies[i]).title("Marker").snippet("Snippet"));
+            boolean minDistance = false;
+            while(!minDistance) {
+                enemies[i] = getRandomLoc();
+                float [] result = new float[1];
+                Location.distanceBetween(lat, lng, enemies[i].latitude, enemies[i].longitude, result);
+
+                if (result[0] > 20.0f) {
+                    minDistance = true;
+                }
+            }
+            addCircle(enemies[i]);
+
         }
         enemiesSet = true;
+    }
+
+    private void addCircle(LatLng enemy) {
+        CircleOptions circleOptions = new CircleOptions()
+                .center(enemy)
+                .radius(10)
+                .fillColor(Color.argb(150, 255, 0, 0))
+                .strokeWidth(3.0f)
+                .strokeColor(Color.argb(80, 0, 0, 0));
+        Circle cirlce = mMap.addCircle(circleOptions);
     }
 
     private LatLng getRandomLoc() {
@@ -113,8 +162,7 @@ public class MapsActivity extends FragmentActivity implements LocationListener{
 
     private double generateRandomDouble(double rangeMin, double rangeMax) {
         Random r = new Random();
-        double randomValue = rangeMin + (rangeMax - rangeMin) * r.nextDouble();
-        return randomValue;
+        return rangeMin + (rangeMax - rangeMin) * r.nextDouble();
     }
 
 
@@ -134,11 +182,17 @@ public class MapsActivity extends FragmentActivity implements LocationListener{
         }
     }
 
+    private void removeGPSText() {
+        gpsText.setVisibility(View.INVISIBLE);
+    }
+
     @Override
     public void onLocationChanged(Location location) {
         lat = location.getLatitude();
         lng = location.getLongitude();
-        latLng = new LatLng(lat, lng);
+        LatLng latLng = new LatLng(lat, lng);
+
+        removeGPSText();
 
         mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
 
@@ -146,7 +200,7 @@ public class MapsActivity extends FragmentActivity implements LocationListener{
 
         if(!enemiesSet) {
             setupEnemies();
-        } else if (enemiesSet) {
+        } else {
             detectEnemy();
         }
     }
