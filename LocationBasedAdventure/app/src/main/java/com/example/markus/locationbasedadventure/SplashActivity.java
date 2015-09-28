90c5251bddfe2b9d243b4955f54d7e86bba2dfd4
maischter <philipp.stefan.mai@googlemail.com>
2015-09-28 23:34:56 +0200

    Final Commit

--- LocationBasedAdventure/app/src/main/java/com/example/markus/locationbasedadventure/SplashActivity.java
+++ LocationBasedAdventure/app/src/main/java/com/example/markus/locationbasedadventure/SplashActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.ImageView;
 
 public class SplashActivity extends Activity {
 
@@ -11,7 +12,13 @@ public class SplashActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        setSplashImage();
         waitALittle();
+    }
+
+    private void setSplashImage() {
+        ImageView splash = (ImageView) findViewById(R.id.splash);
+        splash.setImageResource(R.drawable.splash);
     }
 
     private void waitALittle() {
